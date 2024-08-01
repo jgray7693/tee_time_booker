4d7aa985f6bfaa35bd132e1421ed397fe9d1f08b
jzacharia <9064844+jzacharia@users.noreply.github.com>
2024-08-01 12:44:42 -0700

    login+open tee time json response, needs to be made readable

--- tee_time.py
+++ tee_time.py
@@ -2,9 +2,7 @@ import sys
 import requests
 import datetime
 
-# user settings, modify in userconfig.txt for now
-# (maybe initialize userconfig.txt via cli on first run? can implement later)
-
+# user settings, modify in userconfig.txt for now. will eventually make this configurable via CLI
 # creating list object 'userconfig' and stripping whitespace
 # global constants initialized from userconfig list
 userconfig = [line.strip() for line in open('userconfig.txt')]
@@ -36,13 +34,12 @@ def userLogin(username, password):
     try:
         response = requests.post(LOGIN_URL, data=payload, headers=headers)
         response.raise_for_status()  # this will raise an HTTPError if the response was an error
-        print("Logged in!")
         print("=================")
-        print("DEBUGGING INFO")
+        print("LOGGED IN!")
         print("=================")
 
         print("URL: ", response.url)
-        print("Account Details: ", response.json())
+        # print("Account Details: ", response.json())
     # error handling 
     except requests.exceptions.HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
@@ -53,15 +50,28 @@ def userLogin(username, password):
     except requests.exceptions.RequestException as req_err:
         print(f"An error occurred: {req_err}")
 
-# return list of all available time slots
+def getOpenTeeTimes(date):
+    url = f"{WEBSITE}/index.php/api/booking/times?time=all&{date}&holes=all&players=0&booking_class=929&schedule_id=1470&schedule_ids%5B%5D=1470&schedule_ids%5B%5D=1490&specials_only=0&api_key=no_limits"
+    response = requests.get(url)
+    try:
+        response.raise_for_status
+        open_tee_times = response.json()
+        return open_tee_times
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred when fetching open tee times: {e}")
+        return None
 
-# find + book best time slot
-
-# find + book least crowded course time slot
 
 def main():
     print("running main()")
-    login_result = userLogin(USERNAME, PASSWORD)
-
+    userLogin(USERNAME, PASSWORD)
+    date = input("Enter the desired date to check for open tee times (MM-DD-YYYY)")
+    open_tee_times = getOpenTeeTimes(date)
+    if open_tee_times:
+        print("Open Times:")
+        for tee_time in open_tee_times:
+            print(tee_time)
+            
+            
 if __name__ == "__main__":
     main()
